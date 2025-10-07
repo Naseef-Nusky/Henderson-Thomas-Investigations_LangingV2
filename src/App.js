ef110d1eb5aecffc9612070f85bca638d0b23af9
Naseef Nusky <naseefnusky09@gmail.com>
2025-10-07 16:32:30 +0530

    feat(mobile): add floating confidential call pill with tap-to-call

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ import WhyChooseSection from './components/WhyChooseSection';
 import ContactSection from './components/ContactSection';
 import ReviewSlider from './components/ReviewSlider';
 import Footer from './components/Footer';
+import MobileCallPill from './components/MobileCallPill';
 
 function App() {
   return (
     <div className="min-h-screen">
       <Header />
+      <MobileCallPill />
       <HeroSection />
       <ServicesSlider />
       <Steps />
